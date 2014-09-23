20962dc006b483a161f7a89066a4a2083dc3e400
Santiago Bassett <santiago.bassett@gmail.com>
2014-09-22 18:05:39 -0700

    Improved generate_ossec to modify changelog when creating packages, and to accept arguments.

--- generate_ossec.sh
+++ generate_ossec.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-# Author: Santiago Bassett 06/19/2014
-# Creates Debian packages using Pbuilder and uploads those to Reprepro 
-# Feel free use and modify this code, but do it at your own risk.
+# Usage:
+# -u updates chroot environments
+# -b builds and signs debian packages
+# -s synchronizes uploading new packages to the public repository
 
+packages=(ossec-hids ossec-hids-agent) # only options available
+codenames=(sid jessie wheezy) # only options available
+architectures=(amd64 i386) # only options available
+ossec_version='2.8'
+signing_key='XXX'
+signing_pass='XXX'
+main_path=/root/ # path to ossec-hids and ossec-hids-agent directories
 logfile=/var/log/generate_ossec.log
 
 # Function to write to LOG_FILE
@@ -10,89 +18,174 @@ write_log()
 {
   while read text
   do 
-      logtime=`date "+%Y-%m-%d %H:%M:%S"`
+      local logtime=`date "+%Y-%m-%d %H:%M:%S"`
       echo $logtime": $text" | tee -a $logfile;
   done
 }
 
-packages=(ossec-hids ossec-hids-agent)
-codenames=(sid jessie wheezy)
-architectures=(amd64 i386)
-ossec_version='2.8'
-signing_key='XXX'
-signing_pass='XXX'
+# Show help function
+show_help()
+{
+  echo "USAGE: Command line arguments available:"
+  echo "-h | --help Displays this help."
+  echo "-u | --update Updates chroot environments."
+  echo "-b | --build Builds debian packages."
+  echo "-s | --sync Synchronizes with the debian repository."
+}
 
+# Reads latest package version from changelog file
+# Argument: changelog_file
+read_package_version()
+{
+  local regex="^ossec-hids[A-Za-z-]* \([0-9]+.*[0-9]*.*[0-9]*-([0-9]+)[A-Za-z]*\)"
+  while read line
+  do
+    if [[ $line =~ $regex ]]; then
+      package_version="${BASH_REMATCH[1]}"
+      break
+    fi
+  done < $1
+}
+
+# Updates changelog file with new codename, date and debdist.
+# Arguments: changelog_file codename
+update_changelog()
+{
+  local changelog_file=$1
+  local changelog_file_tmp="${changelog_file}.tmp"
+  local codename=$2
+
+  if [ $codename = "sid" ]; then
+    local debdist="unstable"
+  elif [ $codename = "jessie" ]; then
+    local debdist="testing"
+  elif [ $codename = "wheezy" ]; then
+    local debdist="stable"
+  fi
+
+  local changelogtime=$(date -R)
+  local last_date_changed=0
+
+  local regex1="^(ossec-hids[A-Za-z-]* \([0-9]+.*[0-9]*.*[0-9]*-[0-9]+)[A-Za-z]*\)"
+  local regex2="( -- [[:alnum:]]*[^>]*>  )[[:alnum:]]*,"
+
+  if [ -f ${changelog_file_tmp} ]; then
+    rm -f ${changelog_file_tmp}
+  fi
+  touch ${changelog_file_tmp}
+
+  IFS='' #To preserve line leading whitespaces
+  while read line
+  do
+    if [[ $line =~ $regex1 ]]; then
+      line="${BASH_REMATCH[1]}$codename) $debdist; urgency=low"
+    fi
+    if [[ $line =~ $regex2 ]] && [ $last_date_changed -eq 0 ]; then
+      line="${BASH_REMATCH[1]}$changelogtime"
+      last_date_changed=1
+    fi
+    echo "$line" >> ${changelog_file_tmp}
+  done < ${changelog_file}
+
+  mv ${changelog_file_tmp} ${changelog_file}
+}
+
+# Update chroot environments
+update_chroots()
+{
+  for codename in ${codenames[@]}
+  do
+    for arch in ${architectures[@]}
+    do
+      echo "Updating chroot environment: ${codename}-${arch}" | write_log
+      DIST=$codename ARCH=$arch pbuilder update
+      echo "Successfully updated chroot environment: ${codename}-${arch}" | write_log   
+    done
+  done
+}
+
+# Build packages
+build_packages()
+{
 for package in ${packages[@]}
 do 
   for codename in ${codenames[@]}
   do
     for arch in ${architectures[@]}
-      do
+    do
+      source_path="${main_path}/${package}/${package}-${ossec_version}"
+      changelog_path="${source_path}/debian/changelog"
+      
+      # Updating changelog file with new codename, date and debdist.
+      update_changelog ${changelog_path} ${codename}
 
-      #Updating chroot environment
-      echo "Updating chroot environment: ${codename}-${arch}" | write_log
-      DIST=$codename ARCH=$arch pbuilder update
-      echo "Successfully updated chroot environment: ${codename}-${arch}" | write_log
+      # Setting up global variable package_version, used for deb_file and changes_file.
+      read_package_version ${changelog_path}      
+      deb_file="${package}_${ossec_version}-${package_version}${codename}_${arch}.deb"
+      changes_file="${package}_${ossec_version}-${package_version}${codename}_${arch}.changes"
 
-      #Setting package_path variable
-      if [ $package = "ossec-hids" ]; then
-        package_path="/root/ossec-hids/ossec-hids-${ossec_version}"
-      elif [ $package = "ossec-hids-agent" ]; then 
-        package_path="/root/ossec-hids-agent/ossec-hids-agent-${ossec_version}"
-      fi
-
-      #Updating changelog before building package and cleaning files
-      if [ $codename = "sid" ]; then
-        debdist="unstable"
-      elif [ $codename = "jessie" ]; then
-        debdist="testing"
-      elif [ $codename = "wheezy" ]; then
-        debdist="stable"
-      fi
-      changelogtime=$(date -R)
-      cd ${package_path}
-      sed -i "s/^ossec-hids.*/${package} (${ossec_version}-1${codename}) ${debdist}\; urgency\=low/" debian/changelog
-      sed -i "s/ -- Santiago Bassett <.*/ -- Santiago Bassett <santiago.bassett@gmail.com>  ${changelogtime}/" debian/changelog
-      rm -f debian/files
-
-      #Building the Debian package
+      # Building the Debian package.
       echo "Building Debian package ${package} for ${codename}-${arch}" | write_log
+      cd ${source_path}
       /usr/bin/pdebuild --use-pdebuild-internal --architecture ${arch} --buildresult /var/cache/pbuilder/${codename}-${arch}/result/ \
       -- --basetgz /var/cache/pbuilder/${codename}-${arch}-base.tgz --distribution ${codename} --architecture ${arch} --aptcache \
       /var/cache/pbuilder/${codename}-${arch}/aptcache/ --override-config
       echo "Successfully built Debian package ${package} for ${codename}-${arch}" | write_log
 
       #Checking that package has at least 50 files to confirm it has been built correctly
-      echo "Checking package ${package}_${ossec_version}-1${codename}_${arch}.deb for ${codename}" | write_log
+      echo "Checking package ${deb_file} for ${codename}" | write_log
       cd /var/cache/pbuilder/${codename}-${arch}/result/
-      files=$(/usr/bin/dpkg --contents ${package}_${ossec_version}-1${codename}_${arch}.deb | wc -l)
-      echo "Package ${package}_${ossec_version}-1${codename}_${arch}.deb for ${codename} has ${files} files" | write_log
+      files=$(/usr/bin/dpkg --contents ${deb_file} | wc -l)
+      echo "Package ${deb_file} for ${codename} has ${files} files" | write_log
       if [ "${files}" -lt "50" ]; then
         echo "Error: Package ${package} for distribution ${codename}-${arch} has ${files} files" | write_log
         echo "Error: Aborting package creation and exiting script" | write_log
         exit 1;
       fi
-      echo "Successfully checked package ${package}_${ossec_version}-1${codename}_${arch}.deb for ${codename}" | write_log
+      echo "Successfully checked package ${deb_file} for ${codename}" | write_log
 
       # Signing Debian package
-      echo "Signing Debian package ${package}_${ossec_version}-1${codename}_${arch}.changes for ${codename}" | write_log
+      echo "Signing Debian package ${changes_file} for ${codename}" | write_log
       /usr/bin/expect -c "
-        spawn debsign --re-sign -k${signing_key} ${package}_${ossec_version}-1${codename}_${arch}.changes
+        spawn debsign --re-sign -k${signing_key} ${changes_file}
         expect -re \".*passphrase.*\"
         send \"${signing_pass}\r\"
         expect -re \".*passphrase.*\"
         send \"${signing_pass}\r\"
         expect -re \".*Successfully.*\"
       "
-      echo "Successfully signed Debian package ${package}_${ossec_version}-1${codename}_${arch}.changes for ${codename}" | write_log
+      echo "Successfully signed Debian package ${changes_file} for ${codename}" | write_log   
+ 
+    done
+  done
+done
+}
+
+# Synchronizes with the external repository, uploading new packages and ubstituting old ones.
+sync_repository()
+{
+for package in ${packages[@]}
+do
+  for codename in ${codenames[@]}
+  do
+    for arch in ${architectures[@]}
+    do
+      source_path="${main_path}/${package}/${package}-${ossec_version}"
+      changelog_path="${source_path}/debian/changelog"
+
+      # Setting up global variable package_version, used for deb_file and changes_file.
+      read_package_version ${changelog_path}
+      deb_file="${package}_${ossec_version}-${package_version}${codename}_${arch}.deb"
+      changes_file="${package}_${ossec_version}-${package_version}${codename}_${arch}.changes"
 
       # Uploading package to repository
-      echo "Uploading package ${package}_${ossec_version}-1${codename}_${arch}.changes for ${codename} to OSSEC repository" | write_log
-      /usr/bin/dupload -f --to ossec ${package}_${ossec_version}-1${codename}_${arch}.changes
-      echo "Successfully uploaded package ${package}_${ossec_version}-1${codename}_${arch}.changes for ${codename} to OSSEC repository" | write_log
+      cd /var/cache/pbuilder/${codename}-${arch}/result/
+      echo "Uploading package ${changes_file} for ${codename} to OSSEC repository" | write_log
+      /usr/bin/dupload -f --to ossec ${changes_file}
+      echo "Successfully uploaded package ${changes_file} for ${codename} to OSSEC repository" | write_log
 
       # Moving package to the right directory at the OSSEC apt repository server
-      echo "Adding package /opt/incoming/${package}_${ossec_version}-1${codename}_${arch}.deb to server repository for ${codename} distribution" | write_log
+      echo "Adding package /opt/incoming/${deb_file} to server repository for ${codename} distribution" | write_log
       remove_package="cd /var/www/repos/apt/debian; reprepro -A ${arch} remove ${codename} ${package}"
       /usr/bin/expect -c "
         spawn ssh root@ossec \"${remove_package}\"
@@ -101,7 +194,7 @@ do
         expect -re \".*passphrase.*\" { send \"${signing_pass}\r\" }
         expect -re \".*deleting.*\"
       "
-      include_package="cd /var/www/repos/apt/debian; reprepro includedeb ${codename} /opt/incoming/${package}_${ossec_version}-1${codename}_${arch}.deb"
+      include_package="cd /var/www/repos/apt/debian; reprepro includedeb ${codename} /opt/incoming/${deb_file}"
       /usr/bin/expect -c "
         spawn ssh root@ossec \"${include_package}\"
         expect -re \"Skipping inclusion.*\" { exit 1 }
@@ -111,10 +204,41 @@ do
         send \"${signing_pass}\r\"
         expect -re \".*Exporting.*\"
       "
-      echo "Successfully added package ${package}_${ossec_version}-1${codename}_${arch}.deb to server repository for ${codename} distribution" | write_log
-
-	  done
+      echo "Successfully added package ${deb_file} to server repository for ${codename} distribution" | write_log
+    done
   done
+done
+}
+
+# Reading command line arguments
+while [[ $# > 0 ]]
+do
+key="$1"
+shift
+
+case $key in
+  -h|--help)
+    show_help
+    exit 0
+    ;;
+  -u|--update)
+    update_chroots
+    shift
+    ;;
+  -b|--build)
+    build_packages
+    shift
+    ;;
+  -s|--sync)
+    sync_repository
+    shift
+    ;;
+  *)
+    echo "Unknown command line argument."
+    show_help
+    exit 0
+    ;;
+  esac
 done
 
 # vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2
